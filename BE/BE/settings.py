fe7d07bf4d49ef12b9ab6b9ab0bdef4662f2b693
hwstar-1204 <115150798+hwstar-1204@users.noreply.github.com>
2024-07-30 13:32:16 +0900

    settings.py db 설정
    mysql 연동

--- BE/BE/settings.py
+++ BE/BE/settings.py
@@ -84,13 +84,28 @@ WSGI_APPLICATION = 'BE.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
+DATABASES = {
+    # debug모드일때
+
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv("DATABASE_NAME"),
+        'USER': os.getenv("DATABASE_USER"),
+        'PASSWORD': os.getenv("DATABASE_PASSWORD"),
+        'HOST': os.getenv("DATABASE_HOST"),
+        'PORT': os.getenv("DATABASE_PORT"),
+       }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
