ac4a954e7cdd23a9d5a59e446879173a65bfcefb
kanghaeun <xgkg0330@jnu.ac.kr>
2024-07-31 20:46:19 +0900

    feat: 불필요한 컴포넌트 라우팅 삭제

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -3,7 +3,6 @@ import MainPage from "./pages/MainPage";
 import Notfound from "./pages/Notfound";
 import QRscan from "./pages/QRscan";
 import MyScanMarket from "./pages/MyScanMarket";
-import MarketDetail from "./pages/MarketDetail";
 import ChatBot from "./pages/ChatBot";
 import Test from "./components/chat/Test";
 const App = () => {
@@ -14,7 +13,6 @@ const App = () => {
         <Route path="/" element={<MainPage />} />
         <Route path="/qrscan" element={<QRscan />} />
         <Route path="/myscanmarket" element={<MyScanMarket />} />
-        <Route path="/marketdetail" element={<MarketDetail />} />
         <Route path="/chat/:marketName" element={<ChatBot />} />
         <Route path="*" element={<Notfound />} />
       </Routes>
